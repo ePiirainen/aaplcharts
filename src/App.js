1c7acedbaa756eb26a77337b270eb8dd90e6bd83
Eetu Piirainen <eetu.piirainen@gmail.com>
2020-10-27 16:31:42 +0200

    Added highcharts. App now displays a chart with mock data in it.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
+import jsonData from './aapl-c.json'
+
+const options = {
+  title: {
+    text: 'Apple stock course'
+}, 
+series: [{
+  data: JSON.parse(JSON.stringify(jsonData))
+}]
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
