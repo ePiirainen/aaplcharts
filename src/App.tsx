4a32de322aa1275094b40bc18e54f2411a47c631
Eetu Piirainen <eetu.piirainen@gmail.com>
2020-10-31 16:02:47 +0200

    Disabled navigator since its not really needed and it was producing bugs google didnt help with.

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import * as Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 
-class App extends Component<{}, {chartOptions: {}}> {
+class App extends Component<{}, { chartOptions: {} }> {
 
-constructor(props: any) {
-super(props);
-this.state = {
-  chartOptions: {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      chartOptions: {
+      }
+    };
   }
-};
-}
 
 
   async componentDidMount() {
     this.callApi()
-      .then(res =>{
+      .then(res => {
         let data = [];
         // Parse through JSON data, put the data into one array as objects
-        for(const object of Object.entries(res['Time Series (Daily)'])) {
+        for (const object of Object.entries(res['Time Series (Daily)'])) {
           let date = new Date(object[0]).getTime();
           let values = new Object(object[1]);
           let objectMap = new Map(Object.entries(values));
@@ -28,99 +28,98 @@ this.state = {
           let low = +objectMap.get('3. low')
           let close = +objectMap.get('4. close')
           let volume = +objectMap.get('5. volume')
-          data.push({date, open, high, low, close, volume})
+          data.push({ date, open, high, low, close, volume })
         }
-        // Sort data so highcharts can read it
-        const sortedData = data.sort((a,b) => a.date - b.date)
         // Parse data for highcharts
         var ohlc = [],
-        volume = [],
-        dataLength = data.length,
-        groupingUnits = [
-          [
-            "week", // unit name
-            [1] // allowed multiples
+          volume = [],
+          dataLength = data.length,
+          groupingUnits = [
+            [
+              "week", // unit name
+              [1] // allowed multiples
+            ],
+            ["month", [1, 2, 3, 4, 6]]
           ],
-          ["month", [1, 2, 3, 4, 6]]
-        ],
-        i = 0;
-      
-      for (i; i < dataLength; i += 1) {
-        ohlc.push([
-          data[i].date, // the date
-          data[i].open, // open
-          data[i].high, // high
-          data[i].low, // low
-          data[i].close // close
-        ]);
-      
-        volume.push([
-          data[i].date, // the date
-          data[i].volume // the volume
-        ]);
-      }
-      // Set options, TODO: Figure out why volume chart is over the other stuff
-      const options = {
+          i = 0;
 
-        title: {
-          text: "AAPL Historical"
-        },
-      
-        yAxis: [
-          {
+        for (i; i < dataLength; i += 1) {
+          ohlc.push([
+            data[i].date, // the date
+            data[i].open, // open
+            data[i].high, // high
+            data[i].low, // low
+            data[i].close // close
+          ]);
+
+          volume.push([
+            data[i].date, // the date
+            data[i].volume // the volume
+          ]);
+        }
+        // Set options. Navigator is disabled since I could not for the life of me figure out why the volume chart was overlapping with it, and google didnt help :(
+        const options = {
+
+          title: {
+            text: "AAPL Historical"
+          },
+          navigator: {
+            enabled: false
+          },
+        
+
+          yAxis: [{
             labels: {
-              align: "right",
+              align: 'right',
               x: -3
             },
             title: {
-              text: "OHLC"
+              text: 'OHLC'
             },
-            height: "60%",
+            height: '60%',
             lineWidth: 2,
             resize: {
               enabled: true
             }
-          },
-          {
+          }, {
             labels: {
-              align: "right",
+              align: 'right',
               x: -3
             },
             title: {
-              text: "Volume"
+              text: 'Volume'
             },
-            top: "65%",
-            height: "50%",
+            top: '65%',
+            height: '35%',
             offset: 0,
             lineWidth: 2
-          }
-        ],
-      
-        tooltip: {
-          split: true
-        },
-      
-        series: [
-          {
-            type: "candlestick",
-            name: "AAPL",
-            data: ohlc,
-            dataGrouping: {
-              units: groupingUnits
-            }
+          }],
+
+          tooltip: {
+            split: true
           },
-          {
-            type: "column",
-            name: "Volume",
-            data: volume,
-            yAxis: 1,
-            dataGrouping: {
-              units: groupingUnits
+
+          series: [
+            {
+              type: "candlestick",
+              name: "AAPL",
+              data: ohlc,
+              dataGrouping: {
+                units: groupingUnits
+              }
+            },
+            {
+              type: "column",
+              name: "Volume",
+              data: volume,
+              yAxis: 1,
+              dataGrouping: {
+                units: groupingUnits
+              }
             }
-          }
-        ]
-      };
-        this.setState({chartOptions: options})
+          ]
+        };
+        this.setState({ chartOptions: options })
       })
       .catch(err => console.log(err));
   };
