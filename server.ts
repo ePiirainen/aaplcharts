3ec27008121290648b3b260ca43002fdc64c0777
Eetu Piirainen <eetu.piirainen@gmail.com>
2020-11-01 10:35:26 +0200

    Fiddling with production mode settings for node backend because backend doesnt build in heroku

--- server.ts
+++ server.ts
@@ -6,10 +6,13 @@ const app: express.Application = express()
 
 const PORT = process.env.PORT || 4200;
 
-//app.use(express.json());
 //production mode
-if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {    res.sendFile(path.join(__dirname = 'client/build/index.html'));  })}
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname = 'client/build/index.html'), {root: __dirname});
+ })
+}
 
 app.use(routes);
 app.listen(PORT, () => {
