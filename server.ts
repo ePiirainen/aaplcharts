722161d55a8aba845985f38f77120279df04360d
Eetu Piirainen <eetu.piirainen@gmail.com>
2020-11-01 10:46:04 +0200

    Removed production specific stuff just to test it out

--- server.ts
+++ server.ts
@@ -7,12 +7,12 @@ const app: express.Application = express()
 const PORT = process.env.PORT || 4200;
 
 //production mode
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
      res.sendFile(path.join(__dirname = 'client/build/index.html'), {root: __dirname});
  })
-}
+}*/
 
 app.use(routes);
 app.listen(PORT, () => {
