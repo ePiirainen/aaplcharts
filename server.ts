30968bb90adc12755a257f4f9fbd80c57f974a70
Eetu Piirainen <eetu.piirainen@gmail.com>
2020-11-01 10:11:45 +0200

    Changed app file structure. Moved react frontend to own folder to ease with running app in heroku

---
+++ server.ts
@@ -0,0 +1,17 @@
+import routes from './client/src/routes';
+import express from 'express';
+const path = require('path')
+
+const app: express.Application = express()
+
+const PORT = process.env.PORT || 4200;
+
+//app.use(express.json());
+//production mode
+if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {    res.sendFile(path.join(__dirname = 'client/build/index.html'));  })}
+
+app.use(routes);
+app.listen(PORT, () => {
+  console.log("Node backend listening on port " + PORT)
+});
